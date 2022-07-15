03f7ff2581c3411d225e8007f775892bab83f3e3
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2022-07-15 16:19:45 -0400

    movment, basic map reading from image, face detection

--- Assets/DieController.cs
+++ Assets/DieController.cs
@@ -4,23 +4,128 @@ using UnityEngine;
 
 public class DieController : MonoBehaviour
 {
+    public GameObject manager;
+    private ManageGame gm;
+    public int[] position;
+    int width, length;
+
+    Dictionary<Vector3, int> sides = new Dictionary<Vector3, int>();
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Set up sides
+        sides.Add(Vector3.up, 2);
+        sides.Add(Vector3.down, 5);
+        sides.Add(Vector3.left, 4);
+        sides.Add(Vector3.right, 3);
+        sides.Add(Vector3.back, 6);
+        sides.Add(Vector3.forward, 1);
+
+
+        gm = manager.GetComponent<ManageGame>();
+        width = gm.width;
+        length = gm.length;
     }
 
+
+
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         GetInput();
+        //Debug.Log(gm.levelData);
+        
     }
 
     void GetInput()
     {
-        if (Input.GetKeyDown("space"))
+        int x = position[0];
+        int y = position[1];
+        
+        if (Input.GetKeyDown("a") && !gm.levelData[x -1, y])
         {
-            print("space key was pressed");
+            x--;
+            MoveLeft();
+            transform.Rotate(0, 0, 90, Space.World);
+
         }
+        if (Input.GetKeyDown("d") && !gm.levelData[x + 1, y])
+        {
+            x++;
+            MoveRight();
+            transform.Rotate(0, 0, -90, Space.World);
+        }
+        if (Input.GetKeyDown("w") && !gm.levelData[x, y + 1])
+        {
+            y++;
+            MoveForward();
+            transform.Rotate(90, 0, 0, Space.World);
+        }
+        if (Input.GetKeyDown("s") && !gm.levelData[x, y - 1])
+        {
+            y--;
+            MoveBack();
+            transform.Rotate(-90, 0, 0, Space.World);
+        }
+
+        position = new int[] { x, y };
+        transform.position = new Vector3(x - width / 2, 1, y - length / 2);
     }
+
+    void MoveBack()
+    {
+        Dictionary<Vector3, int> newSides = new Dictionary<Vector3, int>(sides);
+        newSides[Vector3.up] = sides[Vector3.forward];
+        newSides[Vector3.back] = sides[Vector3.up];
+        newSides[Vector3.down] = sides[Vector3.back];
+        newSides[Vector3.forward] = sides[Vector3.down];
+        newSides[Vector3.left] = sides[Vector3.left];
+        newSides[Vector3.right] = sides[Vector3.right];
+        Debug.Log(sides[Vector3.up] + " => " + newSides[Vector3.up]);
+        sides = newSides;
+        
+    }
+
+    void MoveForward()
+    {
+        Dictionary<Vector3, int> newSides = new Dictionary<Vector3, int>(sides);
+        newSides[Vector3.up] = sides[Vector3.back];
+        newSides[Vector3.back] = sides[Vector3.down];
+        newSides[Vector3.down] = sides[Vector3.forward];
+        newSides[Vector3.forward] = sides[Vector3.up];
+        newSides[Vector3.left] = sides[Vector3.left];
+        newSides[Vector3.right] = sides[Vector3.right];
+        Debug.Log(sides[Vector3.up] + " => " + newSides[Vector3.up]);
+        sides = newSides;
+    }
+
+    void MoveLeft()
+    {
+        Dictionary<Vector3, int> newSides = new Dictionary<Vector3, int>(sides);
+        newSides[Vector3.up] = sides[Vector3.right];
+        newSides[Vector3.left] = sides[Vector3.up];
+        newSides[Vector3.down] = sides[Vector3.left];
+        newSides[Vector3.right] = sides[Vector3.down];
+        newSides[Vector3.forward] = sides[Vector3.forward];
+        newSides[Vector3.back] = sides[Vector3.back];
+        Debug.Log(sides[Vector3.up] + " => " + newSides[Vector3.up]);
+        sides = newSides;
+
+    }
+
+    void MoveRight()
+    {
+        Dictionary<Vector3, int> newSides = new Dictionary<Vector3, int>(sides);
+        newSides[Vector3.up] = sides[Vector3.left];
+        newSides[Vector3.left] = sides[Vector3.down];
+        newSides[Vector3.down] = sides[Vector3.right];
+        newSides[Vector3.right] = sides[Vector3.up];
+        newSides[Vector3.forward] = sides[Vector3.forward];
+        newSides[Vector3.back] = sides[Vector3.back];
+        Debug.Log(sides[Vector3.up] + " => " + newSides[Vector3.up]);
+        sides = newSides;
+    }
+
 }
