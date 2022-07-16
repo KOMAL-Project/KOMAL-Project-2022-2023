080629a1b9bb3b3b606b93a4ebb61786c42fbb18
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2022-07-15 21:48:21 -0400

    finished the pull request from hell

--- Assets/Scripts/DieController.cs
+++ Assets/Scripts/DieController.cs
@@ -13,7 +13,7 @@ public class DieController : MonoBehaviour
 
     public Vector2Int position = new Vector2Int();
 
-    AudioSource move;
+ 
 
 
     public Dictionary<Vector3, int> sides = new Dictionary<Vector3, int>();
@@ -23,7 +23,7 @@ public class DieController : MonoBehaviour
     void Start()
     {
 
-        move = GetComponentInChildren<AudioSource>();
+       
         
         // Set up sides
         sides.Add(Vector3.up, 2);
@@ -47,64 +47,6 @@ public class DieController : MonoBehaviour
         GetInput();
         //Debug.Log(gm.levelData);
         
-    }
-
-    void GetInput()
-    {
-        int x = position.x;
-        int y = position.y;
-
-        string[] keys = new string[] { "w", "a", "s", "d" };
-
-        CameraScript cs = cameraObj.GetComponent<CameraScript>();
-
-        //Debug.Log(1 + cs.side);
-
-        if (Input.GetKeyDown(keys[(1 + cs.side)%4]) && !gm.levelData[x -1, y])
-        {
-            x--;
-            MoveLeft();
-            transform.Rotate(0, 0, 90, Space.World);
-
-            move.Play();
-            
-
-        }
-        if (Input.GetKeyDown(keys[(3 + cs.side) % 4]) && !gm.levelData[x + 1, y])
-        {
-            x++;
-            MoveRight();
-            transform.Rotate(0, 0, -90, Space.World);
-
-            move.Play();
-
-        }
-        if (Input.GetKeyDown(keys[(0 + cs.side) % 4]) && !gm.levelData[x, y + 1])
-        {
-            y++;
-            MoveForward();
-            transform.Rotate(90, 0, 0, Space.World);
-
-            move.Play();
-
-        }
-        if (Input.GetKeyDown(keys[(2 + cs.side) % 4]) && !gm.levelData[x, y - 1])
-        {
-            y--;
-            MoveBack();
-            transform.Rotate(-90, 0, 0, Space.World);
-
-        }
-
-        position = new Vector2Int ( x, y );
-
-            move.Play();
-        }
-
-        position = new Vector2Int(x, y);
-        Debug.Log(position);
-
-        transform.position = new Vector3(x - width / 2, 1, y - length / 2);
     }
 
     void MoveBack()
@@ -182,6 +124,65 @@ public class DieController : MonoBehaviour
         sides = newSides;
 
     }
+
+    void GetInput()
+    {
+        int x = position.x;
+        int y = position.y;
+
+        string[] keys = new string[] { "w", "a", "s", "d" };
+
+        CameraScript cs = cameraObj.GetComponent<CameraScript>();
+
+        //Debug.Log(1 + cs.side);
+
+        if (Input.GetKeyDown(keys[(1 + cs.side) % 4]) && !gm.levelData[x - 1, y])
+        {
+            x--;
+            MoveLeft();
+            transform.Rotate(0, 0, 90, Space.World);
+
+        
+
+
+        }
+        if (Input.GetKeyDown(keys[(3 + cs.side) % 4]) && !gm.levelData[x + 1, y])
+        {
+            x++;
+            MoveRight();
+            transform.Rotate(0, 0, -90, Space.World);
+
+           
+
+        }
+        if (Input.GetKeyDown(keys[(0 + cs.side) % 4]) && !gm.levelData[x, y + 1])
+        {
+            y++;
+            MoveForward();
+            transform.Rotate(90, 0, 0, Space.World);
+
+           
+        }
+        if (Input.GetKeyDown(keys[(2 + cs.side) % 4]) && !gm.levelData[x, y - 1])
+        {
+            y--;
+            MoveBack();
+            transform.Rotate(-90, 0, 0, Space.World);
+
+        }
+
+        position = new Vector2Int(x, y);
+
+      
+
+        position = new Vector2Int(x, y);
+        Debug.Log(position);
+
+        transform.position = new Vector3(x - width / 2, 1, y - length / 2);
+    }
+    
+
+    
 
     void MoveRight()
     {
