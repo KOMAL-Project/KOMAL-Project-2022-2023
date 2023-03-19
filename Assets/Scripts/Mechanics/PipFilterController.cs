92d11b3c117e872838c1c151cd1478b1b7615185
Potality <61854720+Potality@users.noreply.github.com>
2023-03-19 13:27:41 -0400

    swap animators in pip filters with tweening

--- Assets/Scripts/Mechanics/PipFilterController.cs
+++ Assets/Scripts/Mechanics/PipFilterController.cs
@@ -8,11 +8,9 @@ public class PipFilterController : MonoBehaviour
     public Texture2D[] topTextures = new Texture2D[7];
     private readonly Sprite[] topSprites = new Sprite[7];
     public SpriteRenderer spriteRenderer;
-    public Animator animator;
+    private LTSeq seq;
     private void Start()
     {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        animator = GetComponent<Animator>();
 
         if (pips > 0)
         {
@@ -20,13 +18,11 @@ public class PipFilterController : MonoBehaviour
             topSprites[pips - 1] = Sprite.Create(topTextures[pips - 1], rect, new Vector2(.5f, .5f));
 
             spriteRenderer.sprite = topSprites[pips - 1];
-            spriteRenderer.gameObject.transform.localScale *= 10;
-            animator.Play("Normal");
         }
         else 
         {
-        spriteRenderer.enabled = false;
-        animator.enabled = false;
+        Disable();
+        DisablePulse();
         }
     }
 
@@ -46,14 +42,40 @@ public class PipFilterController : MonoBehaviour
         return topSprites[type - 1];
     }
 
-    public void Enable() 
-    {
+    public void Enable() {
         spriteRenderer.enabled = true;
-        animator.enabled = true;
     }
-
     public void Disable() {
         spriteRenderer.enabled = false;
-        animator.enabled = false;
     }
+    public void EnablePulse() 
+    {
+        if (!LeanTween.isTweening(gameObject) && pips > 0)
+        {
+            StartCoroutine(Pulse());
+        }
+    }
+    public void DisablePulse()
+    {
+        LeanTween.cancel(gameObject);
+        transform.localScale = Vector3.one * 10;
+        spriteRenderer.material.color = Color.white;
+    }
+
+    public IEnumerator Pulse() { //begins the pulsing
+        yield return new WaitForSeconds(0.5f);
+
+        seq = LeanTween.sequence();
+        seq.append(LeanTween.scale(gameObject, Vector3.one * 10, 0.75f).setEaseInOutQuad());
+        seq.insert(LeanTween.alpha(gameObject, 0, 0.75f).setEaseInOutQuad());
+        seq.append(LeanTween.scale(gameObject, Vector3.one * 5, 0.75f).setEaseInOutQuad().setDelay(1.2f));
+        seq.insert(LeanTween.alpha(gameObject, 1, 0.75f).setEaseInOutQuad());
+        seq.append(LeanTween.value(gameObject, transform.position, transform.position, 0f).setOnComplete( () => { //repeats the whole sequence
+            StartCoroutine(Pulse());
+        }).setDelay(0.05f));
+    }
+
+    private void Update() {
+    }
+
 }
