bdb3fa40686052a1bd46743cd1b3722d6a944ab6
KingDee241 <57509042+KingDee241@users.noreply.github.com>
2023-04-21 21:25:42 -0400

    new toggle switch and block colors

---
+++ Assets/Scripts/FlavorText.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlavorText : MonoBehaviour
+{
+    public Dictionary<string, string> flavorText = new Dictionary<string, string>
+    {
+        {"1-1" , "Touch the yellow panel" },
+        {"1-2" , "Simply a-maze-ing" },
+        {"1-3" , "Intro to Switches" },
+        {"1-4" , "Now there's two!" },
+        {"1-5" , "Clear some space" },
+        {"1-6" , "Intro to Charges" },
+        {"1-7" , "Use a Charge to get a Charge" },
+        {"1-8" , "Power of 4" },
+        {"1-9" , "Switches and Charges" },
+        {"1-10", "Long-distance Charge travel" },
+        {"1-11", "The Box of Fun" },
+        {"1-12", "Finale - 3 Walls" },
+
+        {"1-b1", "A Small(?) Box" },
+        {"1-b2", "Professional Charge carrying" },
+        {"1-b3", "The Power of 6" },
+        {"1-b4", "Finale - Plan 10 Moves Ahead" },
+
+        {"2-1" , "Intro to Power Charges" },
+        {"2-2" , "One Charge, one Card" },
+        {"2-3" , "Focus on one at a time" },
+        {"2-4" , "Square Charges, squared" },
+        {"2-5" , "Remember you can Undo" },
+        {"2-6" , "Plan your route" },
+        {"2-7" , "Doubly Blocked" },
+        {"2-8" , "Cycling Sides" },
+        {"2-9" , "Conserve your space" },
+        {"2-10", "A lot of work for one switch" },
+        {"2-11", "Xs, Xs everywhere" },
+        {"2-12", "Finale - Good Luck" },
+
+        {"2-b1", "Remember the Undo button?" },
+        {"2-b2", "Lots of Power Charges" },
+        {"2-b3", "How hard can it be?" },
+        {"2-b4", "Finale - Use the Red Route" },
+
+        {"3-1" , "Intro to Toggle Blocks" },
+        {"3-2" , "Flip Flopping" },
+        {"3-3" , "A long and narrow road" },
+        {"3-4" , "Two Possibilities" },
+        {"3-5" , "One-Way Toggles" },
+        {"3-6" , "Disco Floor" },
+        {"3-7" , "Switches and Toggles" },
+        {"3-8" , "Switches and Charges" },
+        {"3-9" , "There and Back Again" },
+        {"3-10", "Putting it all together" },
+        {"3-11", "Flip Flopping some more" },
+        {"3-12", "Finale - Routing is Key" },
+
+        {"3-b1", "The Home Stretch" },
+        {"3-b2", "Just keep moving" },
+        {"3-b3", "Even more a-maze-ing" },
+        {"3-b4", "KOMAL" },
+
+    };
+}
